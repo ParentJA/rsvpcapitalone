50ebbad33bf2a42052756037b12fe69950f0f2d6
Jason Parent <jason.a.parent@gmail.com>
2014-07-04 00:37:31 -0400

    Finished API backend functionality. Finished home page/form functionality. Created directives for the 'thank you' and 'waitlist' modals.
    Still need to create modals and handle calls on the client side for waitlisting. Also, need to set up automated email.

--- reservations/static/app/app.js
+++ reservations/static/app/app.js
@@ -1,12 +1,92 @@
 (function() {
 
-    var rsvpCtrl = function($scope) {
+    var rsvpCtrl = function($scope, $http, $log, apiReservationsUrl) {
+        $scope.data = {};
 
+        $scope.submit = function() {
+            var reservationData = {
+                "first_name": $scope.data.firstName,
+                "last_name": $scope.data.lastName,
+                "address": $scope.data.address,
+                "email": $scope.data.email,
+                "num_attending": $scope.data.numAttending
+            };
+
+            var isComplete = true;
+
+            angular.forEach(reservationData, function(value, key) {
+                isComplete = isComplete && angular.isDefined(value);
+            });
+
+            if (isComplete) {
+                $scope.data.isFormIncomplete = false;
+
+                $http({
+                    method: "POST",
+                    url: apiReservationsUrl,
+                    data: reservationData,
+                    xsrfHeaderName: "X-CSRFToken",
+                    xsrfCookieName: "csrftoken"
+                }).success($scope.onSuccess).error($scope.onError);
+
+                //$http.post(apiReservationsUrl, reservationData).success(onSuccess).error(onError);
+            }
+            else {
+                $scope.data.isFormIncomplete = true;
+            }
+        };
+
+        $scope.onSuccess = function(data) {
+            $scope.data.reservationId = data.id;
+            $scope.data.reservationEmail = data.email;
+
+            //Reservations are full, launch waitlist modal...
+            if (data.waitlisted == false) {
+                $("#waitlist-modal").modal("show");
+            }
+
+            //Reservation was successful or user already registered...
+            else {
+                $("#thank-you-modal").modal("show");
+            }
+        };
+
+        $scope.onError = function(data) {
+
+        };
     };
 
-    rsvpCtrl.$inject = ["$scope"];
+    rsvpCtrl.$inject = ["$scope", "$http", "$log", "apiReservationsUrl"];
+
+    var thankYouModal = function() {
+        return {
+            restrict: "E",
+            replace: true,
+            templateUrl: "/reservations/views/thank-you-modal.html"
+        };
+    };
+
+    var waitlistModalCtrl = function($scope, $http, apiReservationsUrl) {
+        $scope.onWaitlistButtonClick = function(event) {
+
+        };
+    };
+
+    waitlistModalCtrl.$inject = ["$scope", "$http", "apiReservationsUrl"];
+
+    var waitlistModal = function() {
+        return {
+            restrict: "E",
+            replace: true,
+            controller: waitlistModalCtrl,
+            templateUrl: "/reservations/views/waitlist-modal.html"
+        };
+    };
 
     angular.module("rsvpApp", [])
-        .controller("rsvpCtrl", rsvpCtrl);
+        .constant("apiReservationsUrl", "/reservations/api/reservations/")
+        .controller("rsvpCtrl", rsvpCtrl)
+        .directive("thankYouModal", thankYouModal)
+        .directive("waitlistModal", waitlistModal);
 
 }());
--- reservations/views.py
+++ reservations/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.http import HttpResponseBadRequest
 from django.http import HttpResponseForbidden
+from django.http import HttpResponseNotAllowed
+from django.http import HttpResponseNotFound
+from django.shortcuts import get_object_or_404
 
 import json
 
@@ -11,14 +14,18 @@ from rsvpcapitalone.settings import MAX_NUM_RESERVATIONS
 
 def api_reservations(request, reservation_id=None):
     if request.method == 'GET':
-        if request.user.is_superuser():
+        if request.user.is_superuser:
             # Get a single reservation by ID...
             if reservation_id:
-                pass
+                selected_reservation = get_object_or_404(Reservation, pk=reservation_id)
+
+                return HttpResponse(selected_reservation.to_json())
 
             # Get all reservations...
             else:
-                pass
+                return HttpResponse(
+                    json.dumps([reservation.to_json() for reservation in Reservation.objects.all()])
+                )
 
         else:
             return HttpResponseForbidden('You must have superuser privileges to access this information.')
@@ -28,7 +35,7 @@ def api_reservations(request, reservation_id=None):
 
         if len(data) == 0:
             try:
-                data = json.dumps(request.body)
+                data = json.loads(request.body)
             except ValueError:
                 return HttpResponseBadRequest('You must call the API with properly formatted parameters.')
 
@@ -36,40 +43,54 @@ def api_reservations(request, reservation_id=None):
         last_name = data.get('last_name')
         address = data.get('address')
         email = data.get('email')
-        num_attending = int(data.get('num_attending'))
-
-        if not all(first_name, last_name, address, email, num_attending):
-            return HttpResponseBadRequest(
-                'You must provide all of the required parameters: first_name, last_name, address, \
-                email, num_attending.'
-            )
+        num_attending = data.get('num_attending')
 
         # Updating specific reservation with waitlist choice...
         if reservation_id:
-            pass
+            if not email:
+                return HttpResponseBadRequest('You must provide the email associated with the reservation ID.')
+
+            try:
+                selected_reservation = Reservation.objects.get(id=reservation_id, email=email)
+                selected_reservation.waitlist = True
+                selected_reservation.save()
+
+                return HttpResponse()
+
+            except Reservation.DoesNotExist:
+                return HttpResponseNotFound('A reservation with the given ID and email was not found.')
 
         # Add a new reservation...
         else:
+            if not all([first_name, last_name, address, email, num_attending]):
+                return HttpResponseBadRequest(
+                    'You must provide all of the required parameters: first_name, last_name, address, \
+                    email, num_attending.'
+                )
+
             current_reservations = Reservation.objects.all()
             num_reservations = len(current_reservations)
-            existing_reservation = current_reservations.filter(email=email)
+            existing_reservation = current_reservations.filter(email=email.lower())
+            selected_reservation = None
 
-            # Only create a new reservation if one does already exist...
-            if not existing_reservation.exists():
+            # Only create a new reservation if one does not already exist...
+            if existing_reservation.exists():
+                selected_reservation = existing_reservation[0]
+            else:
                 num_reservations += 1
-                existing_reservation = Reservation.objects.create(
+                selected_reservation = Reservation.objects.create(
                     first_name=first_name,
                     last_name=last_name,
                     address=address,
-                    email=email,
-                    num_attending=min(2, max(1, num_attending))
+                    email=email.lower(),
+                    num_attending=min(2, max(1, int(num_attending)))
                 )
 
             # Make sure the maximum number of reservations has not been exceeded...
             if num_reservations > MAX_NUM_RESERVATIONS:
-                existing_reservation.waitlisted = False
-                existing_reservation.save()
+                selected_reservation.waitlisted = False
+                selected_reservation.save()
 
-            return HttpResponse(existing_reservation.to_json())
+            return HttpResponse(selected_reservation.to_json())
 
-    return HttpResponse()
+    return HttpResponseNotAllowed(['GET', 'POST'])
